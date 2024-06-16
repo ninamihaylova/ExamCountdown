80450799cf1b97d1d3ebbe92953454a24ed5a75e
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 23:14:27 +0300

    Initial commit

---
+++ ExamCountdown/Program.cs
@@ -0,0 +1,16 @@
+﻿class ExamCountdown 
+{
+    public static void Main(string[] args)
+    {
+        int d = int.Parse(Console.ReadLine());
+     
+
+        for (int i = d; i >=1; i-=1) 
+        {
+            Console.WriteLine($"{i} days before the exam",i);
+        }
+       
+        Console.WriteLine("The exam has come");
+    }
+    
+}
